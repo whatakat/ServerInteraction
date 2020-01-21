276fae5407c75c9ff54e9d7226cbd657eb6db969
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-21 22:36:16 +0300

    build.gradle JavaVersion for lambda

--- app/src/main/java/com/bankmtk/serverinteraction/MainActivity.java
+++ app/src/main/java/com/bankmtk/serverinteraction/MainActivity.java
@@ -3,12 +3,25 @@ package com.bankmtk.serverinteraction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mInfoTextView;
+    private ProgressBar progressBar;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.edit_text);
+        mInfoTextView = (TextView) findViewById(R.id.tvLoad);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        Button btnLoad = (Button)findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener((v)->onClick());
     }
+    public void onClick()
 }

339bdae5b5be3925b17bd013bf4b78f582f8b709
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-23 22:29:39 +0300

    add SugarModel

---
+++ app/src/main/java/com/bankmtk/serverinteraction/model/SugarModel.java
@@ -0,0 +1,15 @@
+package com.bankmtk.serverinteraction.model;
+import com.orm.SugarRecord;
+
+public class SugarModel extends SugarRecord{
+    private String login;
+    private String userId;
+    private String avatarUrl;
+    public SugarModel(){}
+    public SugarModel(String login, String userId, String avatarUrl){
+        this.login = login;
+        this.userId = userId;
+        this.avatarUrl = avatarUrl;
+    }
+
+}

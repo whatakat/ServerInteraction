1c71e06fe161af913bd2d1ce33f5f5b94fac7fb3
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-24 22:31:43 +0300

    onCreate

---
+++ app/src/main/java/com/bankmtk/serverinteraction/OrmApp.java
@@ -0,0 +1,17 @@
+package com.bankmtk.serverinteraction;
+
+import android.app.Application;
+
+import com.orm.SugarContext;
+
+public class OrmApp extends Application {
+    private static final String DATABASE_NAME = "DATABASE_USER_GIT";
+    public static MyDatabase database;
+    public static OrmApp INSTANCE;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SugarContext.init(this);
+    }
+}

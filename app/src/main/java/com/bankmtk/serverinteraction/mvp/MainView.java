55c1871b5d450f4c46f528e3df1e3e9fcb0c81fd
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-24 23:00:13 +0300

    add MainView

---
+++ app/src/main/java/com/bankmtk/serverinteraction/mvp/MainView.java
@@ -0,0 +1,12 @@
+package com.bankmtk.serverinteraction.mvp;
+
+@StateStrategyType(AddToEndSingleStrategy.class)
+public interface MainView {
+    void showAvatar(String avatarUrl);
+    void showError(String message);
+    void setUsername(String username);
+    void showLoading();
+    void hideLoading();
+    void updateRepoList();
+    void init();
+}

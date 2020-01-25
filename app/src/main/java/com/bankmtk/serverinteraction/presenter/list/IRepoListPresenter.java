de5e846eba056870a5620f109227699d602af4fd
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-25 15:21:57 +0300

    interface

--- app/src/main/java/com/bankmtk/serverinteraction/presenter/list/IRepoListPresenter.java
+++ app/src/main/java/com/bankmtk/serverinteraction/presenter/list/IRepoListPresenter.java
@@ -1,4 +1,12 @@
 package com.bankmtk.serverinteraction.presenter.list;
 
-public class IRepoListPresenter {
+import com.bankmtk.serverinteraction.mvp.view.item.RepoItemView;
+
+import io.reactivex.subjects.PublishSubject;
+
+public interface IRepoListPresenter {
+
+    PublishSubject<RepoItemView> getClickSubject();
+    void bindView(RepoItemView rowView);
+    int getRepoCount();
 }

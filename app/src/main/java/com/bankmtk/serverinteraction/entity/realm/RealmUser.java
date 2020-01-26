567fc95bc55172563dbcb5655496a85ada0186e8
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-26 20:41:45 +0300

    add RealmUser

---
+++ app/src/main/java/com/bankmtk/serverinteraction/entity/realm/RealmUser.java
@@ -0,0 +1,43 @@
+package com.bankmtk.serverinteraction.entity.realm;
+
+import androidx.room.PrimaryKey;
+
+public class RealmUser extends RealmObject{
+    @PrimaryKey
+    private String login;
+    private String avatarUrl;
+    private String reposUrl;
+    private RealmList<RealmRepository> repos = new RealmList<>();
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getReposUrl() {
+        return reposUrl;
+    }
+
+    public void setReposUrl(String reposUrl) {
+        this.reposUrl = reposUrl;
+    }
+
+    public RealmList<RealmRepository> getRepos() {
+        return repos;
+    }
+
+    public void setRepos(RealmList<RealmRepository> repos) {
+        this.repos = repos;
+    }
+}

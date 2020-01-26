1d17ff3344ff4a0bfc921a589c0679d1b810fb51
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-26 20:40:12 +0300

    add RealmRepository

---
+++ app/src/main/java/com/bankmtk/serverinteraction/entity/realm/RealmRepository.java
@@ -0,0 +1,25 @@
+package com.bankmtk.serverinteraction.entity.realm;
+
+import androidx.room.PrimaryKey;
+
+public class RealmRepository extends RealmObject {
+    @PrimaryKey
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

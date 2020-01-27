7a4c994bd95f9ce35d6bf3a2693b96cef9d4261d
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-27 21:29:42 +0300

    refactor App

---
+++ app/src/main/java/com/bankmtk/serverinteraction/App.java
@@ -0,0 +1,33 @@
+package com.bankmtk.serverinteraction;
+
+import android.app.Application;
+
+import com.bankmtk.serverinteraction.entity.room.db.UserDatabase;
+import com.orm.SugarContext;
+
+import timber.log.Timber;
+
+public class App extends Application {
+    static private App instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        Timber.plant(new Timber.DebugTree());
+        UserDatabase.create(this);
+        Paper.init(this);
+
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build();
+
+        Realm.setDefaultConfiguration(config);
+    }
+
+    public static App getInstance() {
+        return instance;
+    }
+}

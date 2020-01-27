a3d262402bce794b3b1c4b15246aa8b3d8a47c51
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-27 21:45:41 +0300

    import realm

--- app/src/main/java/com/bankmtk/serverinteraction/entity/realm/RealmUser.java
+++ app/src/main/java/com/bankmtk/serverinteraction/entity/realm/RealmUser.java
@@ -1,6 +1,9 @@
 package com.bankmtk.serverinteraction.entity.realm;
 
 import androidx.room.PrimaryKey;
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
 public class RealmUser extends RealmObject{
     @PrimaryKey
